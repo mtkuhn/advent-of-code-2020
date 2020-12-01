db616e134763af646b89489b91c485d46519d34b
mkuhn <matt.t.kuhn@gmail.com>
2020-12-01 07:49:05 -0700

    Day 1, Part 1 - Find two numbers that add up to 2020, print the product of the two.

---
+++ src/main/kotlin/day1/Day1.kt
@@ -0,0 +1,26 @@
+package day1
+
+import java.io.File
+import java.util.stream.Collectors
+
+fun main() {
+
+    val total = 2020
+    val entries = File("src/main/resources/day1_input.csv").readLines().stream()
+            .map{ it.toInt() }
+            .collect(Collectors.toList())
+    //todo: there's probably a lot of room for optimization at this point
+    val firstResult: Int? = entries.firstOrNull {
+        entries.minusElement(it).contains(total - it)
+    }
+
+    if(firstResult == null) {
+        println("No solution.")
+    } else {
+        val secondResult = 2020-firstResult
+        val solution = firstResult*secondResult
+        println("$firstResult and $secondResult add up to $total. Solution is $solution.")
+    }
+
+}
+
