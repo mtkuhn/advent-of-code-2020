ae0835a05e83b912db91dd3cae59bdc12d696456
mkuhn <matt.t.kuhn@gmail.com>
2020-12-02 09:00:13 -0700

    Day 2, Part 2 -- New version of the count to instead check characters in positions rather than counts

--- readme.md
+++ readme.md
@@ -23,3 +23,9 @@ There's only a few steps here:
 1. Parse the line into usable values. For readability, I opted to parse each line into a data 
 class and stored the min and max as an IntRange.
 2. Validate each parsed line. This was a simple comparison of the char count to the IntRange.
+
+### Part 2
+My part 1 design fit with this fairly well. I opted to refactor the data class
+slightly to use a min and max value rather than range, as it makes usage for part 2
+more clear. Other than that I simply had to update my evaluation function to use
+the data differently.
--- src/main/kotlin/day2/Day2Part2.kt
+++ src/main/kotlin/day2/Day2Part2.kt
@@ -7,9 +7,7 @@ fun main() {
     val lines = File("src/main/resources/day2_input.csv").readLines().asSequence()
     val count = lines.map { it.split("-", " ", ": ") }
         .map { split -> PolicyAndPassword(split[0].toInt(), split[1].toInt(), split[2][0], split[3]) }
-        .count { pp -> pp.password.count { it == pp.letter } in pp.min..pp.max }
+        .count { pp -> (pp.password[pp.min-1] == pp.letter).xor(pp.password[pp.max-1] == pp.letter) }
     println(count)
 
 }
-
-data class PolicyAndPassword(val min: Int, val max: Int, val letter: Char, val password: String)
