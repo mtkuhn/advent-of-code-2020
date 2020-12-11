1006e73ab0194cbad730936e17ea63647c3353cd
mkuhn <matt.t.kuhn@gmail.com>
2020-12-11 08:50:18 -0700

    Day 11 - Seating System -- Code Cleanup

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -19,40 +19,39 @@ fun part2() {
     println(input.countOccupiedSeats()) //2111
 }
 
+operator fun Pair<Int, Int>.plus(pos: Pair<Int, Int>) = first+pos.first to second+pos.second
+
+operator fun Pair<Int, Int>.times(scale: Int) = first*scale to second*scale
+
 class SeatingGrid(private var data: List<String>) {
 
-    private fun isInBounds(x: Int, y: Int): Boolean = x in (data.indices) && y in (data[0].indices)
+    private operator fun get(pos: Pair<Int, Int>): Char = data[pos.first][pos.second]
 
-    private fun getAdjacentChars(x: Int, y: Int) =
-            sequenceOf((x-1 to y-1), (x-1 to y), (x-1 to y+1), (x to y-1), (x to y+1), (x+1 to y-1), (x+1 to y), (x+1 to y+1))
-                    .filter { isInBounds(it.first, it.second) }
-                    .map { data[it.first][it.second] }
+    private fun isInBounds(pos: Pair<Int, Int>): Boolean =
+            pos.first in (data.indices) && pos.second in (data[0].indices)
 
-    private fun getVisibleChars(x: Int, y: Int) =
-            sequenceOf((-1 to -1), (-1 to 0), (-1 to +1), (0 to -1), (0 to +1), (+1 to -1), (+1 to 0), (+1 to +1))
-                    .map { slope -> getFirstCharVisibleInSlope(slope.first, slope.second, x, y) }
+    private fun getAdjacentChars(pos: Pair<Int, Int>) =
+            directions.map { it+pos }.filter { isInBounds(it) }.map { this[it] }
 
-    private fun getFirstCharVisibleInSlope(rise: Int, run: Int, x: Int, y: Int): Char =
+    private fun getVisibleChars(pos: Pair<Int, Int>) =
+            directions.map { slope -> getFirstCharVisibleInSlope(slope, pos) }
+
+    private fun getFirstCharVisibleInSlope(slope: Pair<Int, Int>, start: Pair<Int, Int>): Char =
             (1..maxOf(data.size, data[0].length)).asSequence().map {
-                val x1 = x+(rise*it)
-                val y1 = y+(run*it)
-                if(isInBounds(x1, y1)) data[x1][y1] else '.'
+                val pos = start+(slope*it)
+                if(isInBounds(pos)) this[pos] else '.'
             }.firstOrNull { it != '.' }?:'.'
 
-    private fun iterateRulesToSteadyState(occToEmptyThreshold: Int, getChars: (Int, Int) -> Sequence<Char>) {
-        var updated = true
-        while(updated) { updated = runRulesAndReturnIsUpdated(occToEmptyThreshold, getChars) }
-    }
-
-    private fun runRulesAndReturnIsUpdated(occToEmptyThreshold: Int, getChars: (Int, Int) -> Sequence<Char>): Boolean {
+    private fun runRulesAndReturnIsUpdated(
+            occToEmptyThreshold: Int, getChars: (Pair<Int, Int>) -> Sequence<Char>): Boolean {
         var updated = false
         data = data.asSequence().mapIndexed { x, line ->
             line.asSequence().mapIndexed { y, char ->
-                if (char == 'L' && !getChars(x, y).any { it == '#' }) {
+                if (char == 'L' && !getChars(x to y).any { it == '#' }) {
                     updated = true
                     '#'
                 }
-                else if (char == '#' && getChars(x, y).count { it == '#' } >= occToEmptyThreshold) {
+                else if (char == '#' && getChars(x to y).count { it == '#' } >= occToEmptyThreshold) {
                     updated = true
                     'L'
                 }
@@ -62,10 +61,20 @@ class SeatingGrid(private var data: List<String>) {
         return updated
     }
 
+    private fun iterateRulesToSteadyState(occToEmptyThreshold: Int, getChars: (Pair<Int, Int>) -> Sequence<Char>) {
+        var updated = true
+        while(updated) { updated = runRulesAndReturnIsUpdated(occToEmptyThreshold, getChars) }
+    }
+
     fun countOccupiedSeats(): Int = data.map { line -> line.count { it == '#' } }.sum()
 
-    fun runPart1() = iterateRulesToSteadyState(4) { x, y -> getAdjacentChars(x, y) }
+    fun runPart1() = iterateRulesToSteadyState(4) { pos -> getAdjacentChars(pos) }
 
-    fun runPart2() = iterateRulesToSteadyState(5) { x, y -> getVisibleChars(x, y) }
+    fun runPart2() = iterateRulesToSteadyState(5) { pos -> getVisibleChars(pos) }
+
+    companion object {
+        private val directions =
+                sequenceOf((-1 to -1), (-1 to 0), (-1 to +1), (0 to -1), (0 to +1), (+1 to -1), (+1 to 0), (+1 to +1))
+    }
 
 }
