16cda9e5dfd3fcbf1bd9ebdb285c5957d0bb3d89
mkuhn <matt.t.kuhn@gmail.com>
2020-12-12 09:29:43 -0700

    Day 12 - Rain Risk -- Manage the movement of a ship  based on an instruction set

---
+++ src/main/kotlin/day12/Day12.kt
@@ -0,0 +1,97 @@
+package day12
+
+import java.io.File
+import kotlin.math.*
+
+
+fun main() {
+    part1()
+    part2()
+}
+
+fun getInstructionFromFileInput(): List<Pair<Char, Long>> =
+        File("src/main/resources/day12_input.txt").readLines()
+                .map { it[0] to it.substring(1).toLong() }
+
+operator fun Pair<Long, Long>.plus(pos: Pair<Long, Long>) = first+pos.first to second+pos.second
+operator fun Pair<Long, Long>.times(scale: Long) = first*scale to second*scale
+
+fun part1() {
+    NavigationState().apply {
+        getInstructionFromFileInput().forEach {
+            when(it.first) {
+                'N' -> moveShipInDirection(it.second, NavigationState.NORTH)
+                'E' -> moveShipInDirection(it.second, NavigationState.EAST)
+                'S' -> moveShipInDirection(it.second, NavigationState.SOUTH)
+                'W' -> moveShipInDirection(it.second, NavigationState.WEST)
+                'L' -> turnShipInDegrees(-it.second)
+                'R' -> turnShipInDegrees(it.second)
+                'F' -> moveShipInCurrentDirection(it.second)
+                else -> error("Unsupported operation: $it")
+            }
+        }
+        println(manhattanDistance())
+    }
+}
+
+fun part2() {
+    NavigationState().apply {
+        getInstructionFromFileInput().forEach {
+            when(it.first) {
+                'N' -> moveWaypointInDirection(it.second, NavigationState.NORTH)
+                'E' -> moveWaypointInDirection(it.second, NavigationState.EAST)
+                'S' -> moveWaypointInDirection(it.second, NavigationState.SOUTH)
+                'W' -> moveWaypointInDirection(it.second, NavigationState.WEST)
+                'L' -> rotateWaypointAroundShip(-it.second)
+                'R' -> rotateWaypointAroundShip(it.second)
+                'F' -> moveShipTowardWaypoint(it.second)
+                else -> error("Unsupported operation: $it")
+            }
+        }
+        println(manhattanDistance())
+    }
+}
+
+class NavigationState(
+        private var shipDirection: Long = 0, //in degrees, 0 is due East
+        private var shipPosition: Pair<Long, Long> = 0L to 0L, //north to East
+        private var waypointPosition: Pair<Long, Long> = 1L to 10L
+) {
+    private fun polarToPosition(polar: Pair<Long, Long>): Pair<Long, Long> =
+            when(polar.second) {
+                NORTH -> polar.first to 0L
+                SOUTH -> -polar.first to 0L
+                EAST -> 0L to polar.first
+                WEST -> 0L to -polar.first
+                else -> error("Does not support anything other than cardinal directions: ${polar.second}")
+            }
+
+    fun moveShipInDirection(distance: Long, degrees: Long) { shipPosition += polarToPosition(distance to degrees) }
+
+    fun moveShipInCurrentDirection(distance: Long) = moveShipInDirection(distance, shipDirection)
+
+    fun turnShipInDegrees(degrees: Long) { shipDirection = Math.floorMod(shipDirection + degrees, 360L) }
+
+    fun moveWaypointInDirection(distance: Long, degrees: Long) { waypointPosition += polarToPosition(distance to degrees) }
+
+    fun moveShipTowardWaypoint(scale: Long) { shipPosition += waypointPosition*scale }
+
+    fun rotateWaypointAroundShip(degrees: Long) {
+        waypointPosition = when(Math.floorMod(degrees, 360)) {
+            0L -> waypointPosition
+            90L -> -waypointPosition.second to waypointPosition.first
+            180L -> -waypointPosition.first to -waypointPosition.second
+            270L -> waypointPosition.second to -waypointPosition.first
+            else -> error("Does not support anything other than cardinal directions: $degrees")
+        }
+    }
+
+    fun manhattanDistance(): Long = abs(shipPosition.first) + abs(shipPosition.second)
+
+    companion object {
+        const val EAST: Long = 0L
+        const val SOUTH: Long = 90L
+        const val WEST: Long = 180L
+        const val NORTH: Long = 270L
+    }
+}
