7ea18510dd9c54a6903a7ea6c72ea21c8b82f6de
mkuhn <matt.t.kuhn@gmail.com>
2020-12-14 08:29:01 -0700

    Day 14 -- Docking Data, Part 2

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -4,6 +4,7 @@ import java.io.File
 
 fun main() {
     part1()
+    part2()
 }
 
 fun getInput(): List<String> =
@@ -13,32 +14,56 @@ fun part1() {
     var mask = 0L to 0L
     var memoryMap = mutableMapOf<Long, Long>()
     getInput().map { it.split(" = ") }
-            .map { s ->
-                if(s[0] == "mask") maskInstruction(s[1])
-                else memInstruction(s[0], s[1])
+            .forEach { s ->
+                if(s[0] == "mask") {
+                    mask = (s[1].replace("X", "0").toLong(2)
+                            to s[1].replace("X", "1").toLong(2))
+                }
+                else {
+                    val addr = "mem\\[([0-9]+)\\]".toRegex().find(s[0])?.groupValues?.get(1)?.toLong()?:-1
+                    memoryMap[addr] = s[1].toLong() or mask.first and mask.second
+                }
             }
-            .forEach { instruction ->
-                if(instruction.address >=0) memoryMap[instruction.address] = maskValue(instruction.p1, mask)
-                else mask = instruction.p1 to instruction.p2
+
+    memoryMap.values.sum().apply { println(this) }
+} //13476250121721
+
+fun part2() {
+    var mask = 0L.toString(2)
+    var memoryMap = mutableMapOf<Long, Long>()
+    getInput().map { it.split(" = ") }
+            .forEach { s ->
+                if(s[0] == "mask") {
+                    mask = s[1]
+                }
+                else {
+                    val addr = "mem\\[([0-9]+)\\]".toRegex().find(s[0])
+                            ?.groupValues?.get(1)?.toLong()?:-1
+                    decodeMemoryAddress(mask, addr).forEach() { a ->
+                        memoryMap[a] = s[1].toLong()
+                    }
+
+                }
             }
 
     memoryMap.values.sum().apply { println(this) }
 }
 
-data class Instruction(val address: Long, val p1: Long, val p2: Long)
+fun decodeMemoryAddress(mask: String, addr: Long): List<Long> {
+    val addresses = mutableListOf(mask.replace("X", "0").toLong(2) or addr)
+    mask.mapIndexedNotNull { idx, char -> if(char == 'X') idx else null }
+            .forEach { i ->
+                addresses.addAll(addresses.map { address -> flipBitAt(address, i) })
+            }
+    return addresses.toList()
+}
 
-fun maskInstruction(str: String) =
-        (str.replace("X", "0").toLong(2) to str.replace("X", "1").toLong(2)) //and then or
-            .let{ b -> Instruction(-1, b.first, b.second) }
+fun flipBitAt(address: Long, i: Int): Long {
+    var addressBits = address.toString(2)
+    repeat(36-addressBits.length) { addressBits = "0$addressBits" } //pad with 0s
+    val charReplacement = if(addressBits[i] == '1') '0' else '1' //flip the bit
+    val newStr = addressBits.slice(0 until i)+charReplacement+
+            addressBits.slice(i+1 until addressBits.length) //piece together
+    return newStr.toLong(2)  //as long
+}
 
-fun memInstruction(assign: String, value: String) =
-        Instruction(
-                "mem\\[([0-9]+)\\]".toRegex().find(assign)?.groupValues?.get(1)?.toLong()?:-1,
-                value.toLong(),
-                0
-        )
-
-fun maskValue(value: Long, mask: Pair<Long, Long>) =
-    value or mask.first and mask.second
-
-//or for 1's, and for 0's
