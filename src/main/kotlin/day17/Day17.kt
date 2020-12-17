63ed4dd793c7fdca93c019391e327fabca8cb52c
mkuhn <matt.t.kuhn@gmail.com>
2020-12-17 12:23:24 -0700

    Day 17: Conway Cubes - generic for N dimensions

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -5,7 +5,7 @@ fun main() {
     part2()
 }
 
-fun getInitialCubes() =
+fun getInitialInput() =
         """
             .#.##..#
             ....#.##
@@ -18,11 +18,11 @@ fun getInitialCubes() =
         """.trimIndent()
 
 fun part1() {
-    var cubeMap = CubeMap3D()
-    getInitialCubes().lines().mapIndexed { y, line ->
+    var cubeMap = AnyDimensionalMap(3)
+    getInitialInput().lines().mapIndexed { y, line ->
         line.mapIndexed { x, char ->
             if(char == '#') {
-                cubeMap[Triple(x, y, 0)] = true
+                cubeMap[listOf(x, y, 0, 0)] = true
             }
         }
     }
@@ -30,13 +30,14 @@ fun part1() {
     repeat(6) { cubeMap = cubeMap.applyRules() }
     println(cubeMap.getActiveCoordinates().count())
 }
+
 
 fun part2() {
-    var cubeMap = CubeMap4D()
-    getInitialCubes().lines().mapIndexed { y, line ->
+    var cubeMap = AnyDimensionalMap(4)
+    getInitialInput().lines().mapIndexed { y, line ->
         line.mapIndexed { x, char ->
             if(char == '#') {
-                cubeMap[Coordinate4D(x, y, 0, 0)] = true
+                cubeMap[listOf(x, y, 0, 0)] = true
             }
         }
     }
@@ -45,101 +46,69 @@ fun part2() {
     println(cubeMap.getActiveCoordinates().count())
 }
 
-class CubeMap3D(
-        private val map: MutableMap<Int, MutableMap<Int, MutableMap<Int, Boolean>>> = mutableMapOf(),
-        private val default: Boolean = false
-) {
-
-    operator fun get(c: Triple<Int, Int, Int>): Boolean = map[c.first]?.get(c.second)?.get(c.third)?:default
-
-    operator fun set(c: Triple<Int, Int, Int>, value: Boolean) {
-        if(map[c.first] == null) map[c.first] = mutableMapOf()
-        if(map[c.first]?.get(c.second) == null) map[c.first]?.set(c.second, mutableMapOf())
-        map[c.first]?.get(c.second)?.set(c.third, value)
-    }
-
-    fun getActiveCoordinates() =
-            map.entries.flatMap { xEntry ->
-                xEntry.value.entries.flatMap { yEntry ->
-                    yEntry.value.entries.map { zEntry ->
-                        Triple(xEntry.key, yEntry.key, zEntry.key)
-                    }
-                }
-            }
-
-    private fun getNeighborsOf(c: Triple<Int, Int, Int>): List<Triple<Int, Int, Int>> =
-            (-1..1).flatMap { x1 ->
-                (-1..1).flatMap { y1 ->
-                    (-1..1).mapNotNull { z1 ->
-                        if (x1 != 0 || y1 != 0 || z1 != 0)
-                            Triple(c.first+x1, c.second+y1, c.third+z1)
-                        else null
-                    }
-                }
-            }
-
-    fun applyRules(): CubeMap3D {
-        val newMap = CubeMap3D()
-        getActiveCoordinates()
-                .flatMap { getNeighborsOf(it) }.toSet()
-                .forEach { c ->
-                    val activeNeighbors = getNeighborsOf(c).count { this[it] }
-                    if((!this[c] && activeNeighbors == 3)
-                            || (this[c] && activeNeighbors in (2..3))) newMap[c] = true
-                }
-        return newMap
-    }
-
+interface DimensionalMap {
+    operator fun get(c: List<Int>): Boolean
+    operator fun set(c: List<Int>, newValue: Boolean)
+    fun getActiveCoordinates(): List<List<Int>>
+    fun getPointAndNeighbors(c: List<Int>): List<List<Int>>
 }
 
-data class Coordinate4D(val x: Int, val y: Int, val z: Int, val w: Int)
+//To be used as the base case in a N-dimensional map
+class OneDimensionalMap(private val map: MutableMap<Int, Boolean> = mutableMapOf(), private val default: Boolean = false): DimensionalMap {
+    override fun get(c: List<Int>): Boolean = map[c[0]]?:default
+    override fun set(c: List<Int>, newValue: Boolean) { map[c[0]] = newValue }
+    override fun getActiveCoordinates(): List<List<Int>> =
+            map.entries.mapNotNull { if(it.value) listOf(it.key) else null }
+    override fun getPointAndNeighbors(c: List<Int>): List<List<Int>> = listOf(c[0]-1, c[0], c[0]+1).map { listOf(it) }
+}
 
-class CubeMap4D(
-        private val map: MutableMap<Int, MutableMap<Int, MutableMap<Int, MutableMap<Int, Boolean>>>> = mutableMapOf(),
+class AnyDimensionalMap(
+        private val dimensions: Int,
+        private val map: MutableMap<Int, DimensionalMap> = mutableMapOf(),
         private val default: Boolean = false
-) {
+): DimensionalMap {
 
-    operator fun get(c: Coordinate4D): Boolean = map[c.x]?.get(c.y)?.get(c.z)?.get(c.w)?:default
+    override operator fun get(c: List<Int>): Boolean =
+            map[c[0]]?.get(c.drop(1))?:default
 
-    operator fun set(c: Coordinate4D, value: Boolean) {
-        if(map[c.x] == null) map[c.x] = mutableMapOf()
-        if(map[c.x]?.get(c.y) == null) map[c.x]?.set(c.y, mutableMapOf())
-        if(map[c.x]?.get(c.y)?.get(c.z) == null) map[c.x]?.get(c.y)?.set(c.z, mutableMapOf())
-        map[c.x]?.get(c.y)?.get(c.z)?.set(c.w, value)
+    override operator fun set(c: List<Int>, newValue: Boolean) {
+        if(map[c[0]] == null) {
+            if(dimensions > 2) map[c[0]] = AnyDimensionalMap(dimensions-1)
+            else map[c[0]] = OneDimensionalMap()
+        }
+        map[c[0]]?.set(c.drop(1), newValue)
     }
 
-    fun getActiveCoordinates() =
-            map.entries.flatMap { xEntry ->
-                xEntry.value.entries.flatMap { yEntry ->
-                    yEntry.value.entries.flatMap { zEntry ->
-                        zEntry.value.entries.map { wEntry ->
-                            Coordinate4D(xEntry.key, yEntry.key, zEntry.key, wEntry.key)
-                        }
-                    }
+    override fun getActiveCoordinates(): List<List<Int>> =
+            map.entries.flatMap { cEntry ->
+                cEntry.value.getActiveCoordinates().map { lowerCoordinates ->
+                    listOf(cEntry.key) + lowerCoordinates
                 }
             }
 
-    private fun getNeighborsOf(c: Coordinate4D): List<Coordinate4D> =
-            (-1..1).flatMap { x1 ->
-                (-1..1).flatMap { y1 ->
-                    (-1..1).flatMap { z1 ->
-                        (-1..1).mapNotNull { w1 ->
-                            if (x1 != 0 || y1 != 0 || z1 != 0 || w1 != 0)
-                                Coordinate4D(c.x + x1, c.y + y1, c.z + z1, c.w + w1)
-                            else null
-                        }
-                    }
+    override fun getPointAndNeighbors(c: List<Int>): List<List<Int>> {
+        var points = (-1..1).map { listOf(c[0]+it) }
+        repeat(dimensions-1) { dimension ->
+            points = points.flatMap { coordinate ->
+                (-1..1).map { newPoint ->
+                    coordinate + (c[dimension+1] + newPoint)
                 }
             }
+        }
+        return points
+    }
 
-    fun applyRules(): CubeMap4D {
-        val newMap = CubeMap4D()
+
+    private fun getNeighborsOf(c: List<Int>) = getPointAndNeighbors(c).filter { it != c }
+
+    fun applyRules(): AnyDimensionalMap {
+        val newMap = AnyDimensionalMap(dimensions)
         getActiveCoordinates()
                 .flatMap { getNeighborsOf(it) }.toSet()
                 .forEach { c ->
                     val activeNeighbors = getNeighborsOf(c).count { this[it] }
-                    if((!this[c] && activeNeighbors == 3)
-                            || (this[c] && activeNeighbors in (2..3))) newMap[c] = true
+                    if((!this[c] && activeNeighbors == 3) || (this[c] && activeNeighbors in (2..3)))
+                        newMap[c] = true
                 }
         return newMap
     }
