a7fee04865eb76582127bd83af284689b8022204
mkuhn <matt.t.kuhn@gmail.com>
2020-12-20 11:08:38 -0700

    Day 19: Monster Messages

--- src/main/kotlin/day19/Day19Part2.kt
+++ src/main/kotlin/day19/Day19Part2.kt
@@ -27,47 +27,29 @@ fun part1b() {
 
 fun part2b() {
     val rules = getRuleMap().toMutableMap()
-    rules[8] = "8: 42 | 42 8".parseRule().second
-    rules[11] = "11: 42 31 | 42 11 31".parseRule().second
+    //rules[8] = "8: 42 | 42 8".parseRule().second
+    //rules[11] = "11: 42 31 | 42 11 31".parseRule().second
 
     rules[8] = "((42)+)"
-    rules[11] = "((42)+(31)+)"
+    
+    var rule11 = (1..100).map {
+        "((42){$it}(31){$it})"
+    }.joinToString(separator = "|", prefix="(", postfix= ")")
+    rules[11] = rule11
 
-    //rules.forEach {println(it)}
-
-    while(rules.values.any { it.contains("\\d".toRegex()) }) {
+    while(rules.values.any { it.contains("\\(\\d+\\)".toRegex()) }) {
         rules.forEach() { ruleMapEntry ->
             "\\(\\d+\\)".toRegex().findAll(ruleMapEntry.value)
                     .map { it.value }
                     .forEach { foundNumber ->
                         val intNumber = foundNumber.replace("[^\\d]".toRegex(), "").toInt()
-                        if(intNumber != ruleMapEntry.key) {
-                            val replacement = rules[intNumber]!!.drop(1).dropLast(1)
-                            rules[ruleMapEntry.key] = ruleMapEntry.value.replace(foundNumber, "($replacement)")
-                        } else {
-                            val before = ruleMapEntry.value.substringBefore(foundNumber)
-                            val after = ruleMapEntry.value.substringAfter(foundNumber)
-                            val replacement = "($before$after)+"
-                            println("rule= ${ruleMapEntry.value}")
-                            println("repl= $replacement")
-                            println("repd= ${ruleMapEntry.value.replace(foundNumber, replacement)}")
-                            rules[ruleMapEntry.key] = "("+ruleMapEntry.value.replace(foundNumber, "")+"+)"
-                        }
+                        val replacement = rules[intNumber]!!.drop(1).dropLast(1)
+                        rules[ruleMapEntry.key] = ruleMapEntry.value.replace(foundNumber, "($replacement)")
                     }
-
-            //8: 42 | 42 8 -> (42)|((42)(8)) -> (42)|((42))+
-            //11: 42 31 | 42 11 31 -> ((42)(31))|((42)(11)(31)) -> ((42)(31))|((42)(31))+
         }
     }
 
-    println("rules iterated")
-
-    //rules.forEach {println(it)}
-
     countMatchingMessages(rules).apply{ println(this) }
-    //470 -- too high
-    //458 -- wrong
-    //418 -- too low
 }
 
 fun getRuleMap(): Map<Int, String> =
