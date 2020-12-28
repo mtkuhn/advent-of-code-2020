86489ad1390f2f7efa00013ada513ddac7e9a828
mkuhn <matt.t.kuhn@gmail.com>
2020-12-28 08:23:47 -0700

    Day 25 cleanup

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -1,34 +1,12 @@
 package day25
 
 fun main() {
-    val pubKeyA = 3418282L
-    val pubKeyB = 8719412L
-    val subjectNumber = 7L
-
-    val key = Key(subjectNumber)
-    val keyA = key.loopUntilValue(pubKeyA)
-    val keyB = key.loopUntilValue(pubKeyB)
-
-    println(Key(keyB.value).loop(keyA.loops))
+    val key = Key(7L)
+    val keyA = generateSequence(key) { it.loop() }.first { it.value == 3418282L }
+    val keyB = generateSequence(key) { it.loop() }.first { it.value == 8719412L }
+    generateSequence(Key(keyB.value)) { it.loop() }.elementAt(keyA.loops).apply { println(this) }
 }
 
 data class Key(val subjectNumber: Long, val loops: Int = 0, val value: Long = 1L) {
-
     fun loop(): Key = Key(subjectNumber, loops+1, (value*subjectNumber)%20201227L)
-
-    fun loop(count: Int): Key {
-        var key = this
-        repeat(count) {
-            key = key.loop()
-        }
-        return key
-    }
-
-    fun loopUntilValue(pubKey: Long): Key {
-        var key = this
-        while(key.value != pubKey) {
-            key = key.loop()
-        }
-        return key
-    }
 }
